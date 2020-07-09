5546d040d84b33d1a293f304ea8d57754868867a
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-07-09 14:03:25 +0000

    Install mongodb and create the first database for the application

--- src/server.js
+++ src/server.js
@@ -1,21 +1,43 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { MongoClient } from 'mongodb';
+
+const app = express();
 
 const articlesInfo = {
   'learn-react': {
     upvotes: 0,
+    comments: [],
   },
   'learn-node': {
     upvotes: 0,
+    comments: [],
   },
-  'my-though-on-resumes': {
+  'my-thoughts-on-resumes': {
     upvotes: 0,
+    comments: [],
   },
 };
 
-const app = express();
-
 app.use(bodyParser.json());
+app.get('/api/articles/:name', async (req, res) => {
+  try {
+    const articleName = req.params.name;
+    const client = await MongoClient.connect('mongodb://localhost:27017', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('client is ', client);
+    const db = client.db('my-blog');
+    const articleInfo = await db
+      .collection('articles')
+      .findOne({ name: articleName });
+    res.status(200).json(articleInfo);
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: 'Error connecting to db', error });
+  }
+});
 
 app.post('/api/articles/:name/upvote', (req, res) => {
   const articleName = req.params.name;
@@ -23,8 +45,16 @@ app.post('/api/articles/:name/upvote', (req, res) => {
   res
     .status(200)
     .send(
-      `${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`
+      `${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!`
     );
+});
+
+app.post('/api/articles/:name/add-comment', (req, res) => {
+  const { username, text } = req.body;
+  const articleName = req.params.name;
+
+  articlesInfo[articleName].comments.push({ username, text });
+  res.status(200).send(articlesInfo[articleName]);
 });
 
 app.listen(8000, () => console.log('Listening on port 8000'));
